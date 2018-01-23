249967a1d283e3261f842ae508de515f22a1c099
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2018-01-23 12:29:36 -0500

    New Database - setup just started

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'cod',
+      'username' => 'drupal',
+      'password' => 'drupal',
+      'host' => '127.0.0.1',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'ihVAU5OBTCGW7UJTrjREEjZ_gl6DnxRbH-8NzOe-V8U';
 
 /**
  * Base URL (optional).
